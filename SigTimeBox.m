e2c22579fefad9aeacfb79c83a37221b2dde89aa
Krista Perks <kperky@gmail.com>
2014-01-24 12:05:13 -0800

    add color options to SigTimeBox

--- SigTimeBox.m
+++ SigTimeBox.m
@@ -1,4 +1,17 @@
  
-function SigTimeBox(hl, sigon, sigoff, ylims)
+function SigTimeBox(hl, sigon, sigoff, ylims,color)
+%sigon and sigoff are indices not real time
 axes(hl);
-patch([sigon sigon sigoff sigoff],[min(ylims) max(ylims) max(ylims) min(ylims)],[-.1 -.1 -.1 -.1], [0.9 0.9 0.9],'EdgeColor','none');
+% if regexp(color,'gray')
+%     colorind = [0.9 0.9 0.9];
+% elseif regexp(color, 'red')
+%     colorind = [1, 0.5, 0.5];
+% elseif regexp(color, 'black')
+%     colorind = [0.5 0.5 0.5];
+% elseif regexp(color, 'blue')
+%     colorind = [0.5, 0.5, 1];
+% end
+patch([sigon sigon sigoff sigoff],...
+    [min(ylims) max(ylims) max(ylims) min(ylims)],...
+    [-.1 -.1 -.1 -.1], color,....
+    'EdgeColor','none','FaceAlpha',0.5);
